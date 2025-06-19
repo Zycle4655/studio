b823fc11bf8c7a6301536bbffe46c39026e36be1
Cristian Mejia <cristinversion2019@gmail.com>
2025-06-19 08:53:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <svg xmlns="http:/

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,3 @@
-import ZycleIcon from '@/components/icons/ZycleIcon';
 import Link from 'next/link';
 
 export default function AuthLayout({
@@ -10,7 +9,6 @@ export default function AuthLayout({
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-background">
       <div className="mb-8 text-center">
         <Link href="/" className="inline-flex items-center gap-2">
-          <ZycleIcon className="w-10 h-10 text-primary" />
           <h1 className="text-5xl font-bold text-primary font-headline">ZYCLE</h1>
         </Link>
       </div>
