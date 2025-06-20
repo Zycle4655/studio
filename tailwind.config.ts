dd8f902f653496d4ed94ebbab796b86e5e37d42f
Cristian Mejia <cristinversion2019@gmail.com>
2025-06-20 04:04:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarContent>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,7 +56,7 @@ export default {
           '5': 'hsl(var(--chart-5))',
         },
         sidebar: {
-          DEFAULT: 'hsl(var(--sidebar-background))',
+          DEFAULT: 'var(--sidebar-background)', // Changed to use var() directly for gradient
           foreground: 'hsl(var(--sidebar-foreground))',
           primary: 'hsl(var(--sidebar-primary))',
           'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
