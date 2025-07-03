f1b380e2b56d18b5baea7c03b9047020f2e7cd93
Cristian Mejia <cristinversion2019@gmail.com>
2025-07-03 10:47:33 +0000

    no se ve la imagen, deja asi,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Oswald:wght@700&display=swap" rel="stylesheet" />
       </head>
-      <body className="bg-leaves-pattern font-body antialiased min-h-screen flex flex-col">
+      <body className="font-body antialiased min-h-screen flex flex-col">
         <AuthProvider>
           {children}
         </AuthProvider>
